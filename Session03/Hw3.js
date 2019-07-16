f81a58907ee1e234e2a80a9ddd9d31bef102bdb4
unknown <nguyendung13072000@gmail.com>
2019-07-16 15:05:47 +0700

    Hw for Session 3

---
+++ Session03/Hw3.js
@@ -0,0 +1,30 @@
+// let menu = ["T-Shirt", "Sweater"];    
+// alert(`Let see what's in here: ${menu}`);
+// for (;;) {
+// let x= prompt("Welcome to our shop, what do you want (C, R, U, D)").toLowerCase();
+// if (x === "c"){
+// let a = prompt("Enter new item:");
+// menu.push(a);
+// alert(`List after fixing: ${menu}`); 
+// continue;
+// }
+// else if (x === "r"){
+//    alert(menu);
+//    continue;
+// }
+// else if (x === "u"){
+//    let  b = Number(prompt("Update position?"));
+//   let c = prompt("What's the fucking stuff you want to change:");
+//     menu.splice(b-1,1,c); 
+//     alert(`List after fixing: ${menu}`);
+//     continue;
+// }
+// else if (x === "d"){
+//     let d = Number(prompt("Delete position?"));
+//     menu.splice(d-1,1);
+//     alert(`List after fixing: ${menu}`);
+//     continue;
+// }
+// else{ break;}
+// }
+// alert(`List after fixing: ${menu}`);
